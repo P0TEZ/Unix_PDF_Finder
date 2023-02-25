f77f30dc920877a6904931c9221c3addac7876b4
P0TEZ <60687263+P0TEZ@users.noreply.github.com>
2023-02-25 21:25:51 +0100

    add methodes
    show/reset history
    
    add dir remove dir show config file and reset it

--- pdfFinder.sh
+++ pdfFinder.sh
@@ -9,9 +9,10 @@ history_file="$HOME/.config/pdf_search/history.txt"
 if [ -f "$config_file" ]; then
     echo "Configuration file found."
 else
-    echo "Configuration file not found. Exiting script."
-    echo "Configuration file not found. Exiting script." >> error.log
-    exit 1
+    echo "Configuration file not found. Creating it."
+    echo "Configuration file not found. Creating it." >> error.log
+    mkdir -p "$HOME/.config/pdf_search"
+    touch "$config_file"
 fi
 
 # Check if history file exists
@@ -25,9 +26,95 @@ else
     touch "$history_file"
 fi
 
+#if the the parameter "-resetConfig" is passed, we reset the configuration file
+if [ "$1" = "-resetConfig" ]; then
+    echo "Resetting configuration file."
+    echo "Resetting configuration file." >> error.log
+    > "$config_file"
+    exit 0
+fi
+
+#if the the parameter "-resetHistory" is passed, we reset the history file
+if [ "$1" = "-resetHistory" ]; then
+    echo "Resetting history file."
+    echo "Resetting history file." >> error.log
+    > "$history_file"
+    exit 0
+fi
+
+#if the parameter "-history" is passed, we display the content of the history file
+if [ "$1" = "-history" ]; then
+    echo "Displaying history file content."
+    echo "Displaying history file content." >> error.log
+    cat "$history_file"
+    exit 0
+fi
+
+#if the parameter "-addDir" is passed, we add the folder to the configuration file
+if [ "$1" = "-addDir" ]; then
+    #if the parameter "-addDir" is passed, we check if the second parameter is not empty
+    if [ -n "$2" ]; then
+        #if the second parameter is not empty, we check if the folder exists
+        if [ -d "$2" ]; then
+            #if the folder exists, we add it to the configuration file
+            echo "Adding folder $2 to the configuration file."
+            echo "Adding folder $2 to the configuration file." >> error.log
+            echo "$2" >> "$config_file"
+            exit 0
+        else
+            #if the folder does not exist, we display an error message
+            echo "Folder $2 does not exist. Exiting script."
+            echo "Folder $2 does not exist. Exiting script." >> error.log
+            exit 1
+        fi
+    else
+        #if the second parameter is empty, we display an error message
+        echo "No folder specified. Exiting script."
+        echo "No folder specified. Exiting script." >> error.log
+        exit 1
+    fi
+fi
+
+#if the parameter "-removeDir" is passed, we remove the folder from the configuration file
+if [ "$1" = "-removeDir" ]; then
+    #if the parameter "-removeDir" is passed, we check if the second parameter is not empty
+    if [ -n "$2" ]; then
+        #if the second parameter is not empty, we check if the folder exists
+        if [ -d "$2" ]; then
+            #if the folder exists, we remove it from the configuration file
+            echo "Removing folder $2 from the configuration file."
+            echo "Removing folder $2 from the configuration file." >> error.log
+
+            #escape every / with a \ to avoid sed errors
+            toRemove=$(echo "$2" | sed 's/\//\\\//g') 2>> error.log
+
+            #remove the folder from the configuration file only if the line starts with the folder name and ends with \n 
+            sed -i "/^$toRemove\$/d" "$config_file" 2>> error.log
+
+
+            exit 0
+        else
+            #if the folder does not exist, we display an error message
+            echo "Folder $2 does not exist. Exiting script."
+            echo "Folder $2 does not exist. Exiting script." >> error.log
+            exit 1
+        fi
+    else
+        #if the second parameter is empty, we display an error message
+        echo "No folder specified. Exiting script."
+        echo "No folder specified. Exiting script." >> error.log
+        exit 1
+    fi
+fi
+
+
 #display the content of the configuration file (for debugging)
 #echo "Configuration file content:"
 #cat "$config_file"
+
+
+
+#The main script part start here ----------------------------------------------
 
 # Function to get list of PDF files from the folders specified in the config file
 get_pdf_files() {
